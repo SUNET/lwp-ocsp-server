fd1369cc5a875c3a69472fe8850cd6bd2ce13e84
Johan Wassberg <jocar@sunet.se>
2024-11-29 14:06:23 +0100

    Better handle unknown and faulty realms

--- ocsp-server.py
+++ ocsp-server.py
@@ -51,13 +51,44 @@ def ocsp_server(realm):
 
         ca_rows = cur.fetchall()
 
-        if len(ca_rows) != 1:
-            return Response("Couldn't fetch key and pem from db", status=500)
+        if len(ca_rows) == 0:
+            message = f"Couldn't find {realm} in db"
+            app.logger.error(message)
+            return Response(message, status=500)
+        elif len(ca_rows) != 1:
+            message = f"Multiple rows for {realm} found in db"
+            app.logger.error(message)
+            return Response(message, status=500)
 
-        ca_key_bytes = bytes(ca_rows[0][0], "utf-8")
-        ca_key = serialization.load_pem_private_key(ca_key_bytes, None)
-        ca_pem_bytes = bytes(ca_rows[0][1], "utf-8")
-        ca_pem = load_pem_x509_certificate(ca_pem_bytes)
+        if ca_rows[0][0]:
+            ca_key_bytes = bytes(ca_rows[0][0], "utf-8")
+            try:
+                ca_key = serialization.load_pem_private_key(ca_key_bytes, None)
+            except ValueError:
+                message = f"Could not decode key for {realm} found in database"
+                app.logger.error(message)
+                return Response(message, status=500)
+            except:
+                message = f"Invalid key for {realm} found in database"
+                app.logger.error(message)
+                return Response(message, status=500)
+        else:
+            message = f"No key for {realm} found in database"
+            app.logger.error(message)
+            return response(message, status=500)
+
+        if ca_rows[0][1]:
+            ca_pem_bytes = bytes(ca_rows[0][1], "utf-8")
+            try:
+                ca_pem = load_pem_x509_certificate(ca_pem_bytes)
+            except:
+                message = f"Invalid cert for {realm} found in database"
+                app.logger.error(message)
+                return Response(message, status=500)
+        else:
+            message = f"No cert for {realm} found in database"
+            app.logger.error(message)
+            return response(message, status=500)
 
         try:
             ocsp_req = ocsp.load_der_ocsp_request(request.data)
